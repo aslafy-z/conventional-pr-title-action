2ce59b07f86bd51b521dd088f0acfb0d7fdac55e
Zadkiel AHARONIAN <zadkiel_aharonian@carrefour.com>
2023-08-10 12:13:44 +0200

    revert: feat(action): migrate from docker to node (#267)
    This reverts commit 6fd10ff03114fe6062f4630ce4e11b08e063c985.

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:19-alpine
+
+ADD package.json package-lock.json /action/
+RUN cd /action && npm ci
+
+ADD src /action/src
+ENTRYPOINT ["node", "/action/src/index.js"]
--- release.config.js
+++ release.config.js
@@ -4,15 +4,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/exec", {
-      prepareCmd: "npm ci && npm run build",
-    }],
-    ["@semantic-release/npm", {
-      npmPublish: false,
-    }],
-    ["@semantic-release/git", {
-      assets: ["package.json", "package-lock.json", "dist"],
-    }],
+    "@semantic-release/git",
     "@semantic-release/github",
   ],
 };
